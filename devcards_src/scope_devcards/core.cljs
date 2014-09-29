a0cd62342c753aff32822e62cadfb72ebe15ec01
Jason Whitlark <jason@whitlark.org>
2014-09-29 11:19:33 -0700

    Latest tweaks.

--- devcards_src/scope_devcards/core.cljs
+++ devcards_src/scope_devcards/core.cljs
@@ -21,43 +21,85 @@
 ;; remember to run lein figwheel and then browse to
 ;; http://localhost:3449/devcards/index.html
 
+;; -------------------- Constants --------------------
+
+(def buffer-max 100)
+(def graph-width 100)
+(def graph-height 30)
+
 ;; -------------------- Helpers --------------------
+
+(defn log [x]
+  (.log js/console x))
 
 (defn by-id [id]
   (.getElementById js/document id))
 
-(defn set-states! [owner desired]
+(defn set-states!
+  "Given a map, for each k/v pair, set! that key in the state to the
+  given value."
+  [owner desired]
   (doall (map #(om/set-state! owner (key %) (val %)) desired)))
 
-;; -------------------- Helpers --------------------
+(def sin #(.sin js/Math %))
 
-(defn calc-start [amp]
-  (str  "M" "0.1,"  (- 15 amp)))
+(defn move-absolute [x y]
+  {:pre [(and (not (nil? x))
+              (not (nil? y)))]}
+  (str "M" x "," y))
+
+(defn line-absolute [x y]
+  {:pre [(and (not (nil? x))
+              (not (nil? y)))]}
+  (str "L" x "," y))
+
+(defn line-relative [x y]
+  {:pre [(and (not (nil? x))
+              (not (nil? y)))]}
+  (str "l" x "," y))
+
+(def close-shape (constantly "Z"))
+
+(defn box [w h]
+  (->> [(move-absolute 0 0)
+        (line-absolute graph-width 0)
+        (line-absolute graph-width graph-height)
+        (line-absolute 0 graph-height)
+        (close-shape)]
+   (str/join " ")))
+
+(defn calc-start
+  "Figure out the correct place to start drawing our graph."
+  [ampl]
+  (move-absolute 0.1 (-> graph-height (/ 2) (- ampl))))
 
 (defn sin-seq
-  "Given a frequency, generate a sequence to use as the :d value of an svg path element."
-  [freq amp]
+  "Given a frequency, generate a sequence to use as the :d value of an
+  svg path element."
+  [freq ampl]
   ;; FIXME: Need to scale these so that low values don't cause jaggies  (jw 14-09-27)
-  (str/join " "
-         (cons (calc-start amp)
-          (for [x (range (* 2 freq))]
-            (str "l" (/ 50 freq) ","
-                 (* amp (.sin js/Math x)))))))
+  (->> (for [x (range (* 2 freq))]
+         (line-relative (/ 50 freq)
+                        (* ampl (sin x))))
+       (cons (calc-start ampl))
+       (str/join " ")))
 
-(defn graph [freq ampl]
+(defn graph
+  "Draw a little sparkline sine-wave graph."
+  [freq ampl]
   [:div [:svg
-         {:style {:width "100px" :height "30px"} }
+         {:style {:width (str graph-width "px")
+                  :height (str graph-height "px")}}
          ;; the frame around the graph
          [:path {:cs "100,100"
-                 :d "M0.0,0.0 L99.5,0.0 L99.5,29.5 L0.5,29.5 Z"
+                 :d (box graph-width graph-height)
                  :stroke "blue" :stroke-width "1"
-                 :fill "white" :fill-opacity "0"}]
+                 :fill "none"}]
          ;; graph itself
          [:path {:cs "100,100"
                  :d (sin-seq freq ampl)
-                  :fill "none" :stroke-width "1" :stroke-opacity "1"
-                 :stroke "green"}]
-         ]])
+                 :stroke "green" :stroke-width "1"
+                 :fill "none"}]]])
 
 (defn simplify-event
   [evt]
@@ -66,11 +108,7 @@
    :type (.-type evt)
    :e evt})
 
-(def buffer-max 100)
-
-(defn log [x]
-  (.log js/console x))
-
+;; handler
 (defn handle-scrubber-evt [cursor owner evt-type evt]
   (case evt-type
     :down (set-states! owner {:start-y (.-offsetY evt)
@@ -87,6 +125,7 @@
               (set-states! owner {:start-x (.-offsetX evt)
                                   :start-y (.-offsetY evt)})))))
 
+;; scrubber mechanics
 (defn scrubbable [cursor owner {:keys [build-fn id]}]
   (reify
     om/IInitState
@@ -97,10 +136,10 @@
        :start-y nil})
     om/IWillMount
     (will-mount [_]
-      (let [mouse-chan (om/get-state owner :mouse-chan)]
+      (let [mouse-chan (om/get-state owner :mouse-chan)
+            handler (partial handle-scrubber-evt cursor owner)]
         (go-loop []
-          (let [[evt-type e] (<! mouse-chan)]
-            (handle-scrubber-evt cursor owner evt-type e))
+          (apply handler (<! mouse-chan))
           (recur))))
     om/IDidMount
     (did-mount [_]
@@ -112,30 +151,29 @@
     om/IRenderState
     (render-state [_ state]
       (sab/html [:div {:id id
-                       :style {:z-index 100
-                               :color (if (:capturing state) "#00f" "#000")
-                               :border-bottom "1px dotted #00f"
-                               :cursor "all-scroll"
-                               :-webkit-user-select "none"}}
+                       :class  "scrubber"
+                       :style {:-webkit-user-select "none"}}
                  build-fn]))))
 
-
+;; Make something scrubbable
 (defn scrubbable-widget [id target cursor owner]
      (reify
        om/IRenderState
        (render-state [_ state]
          (sab/html
-          [:div {:class "container"}
+          [:div
            (om/build scrubbable
                      (:scrubbable cursor)
                      {:opts {:id id
                              :build-fn (om/build target (:scrubbable cursor))}})]))))
 
+;; View components
 (defn x-y-viewer [cursor owner]
   (om/component
    (sab/html
-    [:div [:em "x: " (:x cursor) ", "]
-     [:em "y: " (:y cursor)]])))
+    [:div
+     "x: " (:x cursor) ", "
+     "y: " (:y cursor)])))
 
 (defn sin-viewer [cursor owner]
   (om/component
@@ -144,14 +182,16 @@
      (sab/html
       (graph freq ampl)))))
 
+;; Final display
 (def app-model
   (atom {:scrubbable {:x 50 :y 15}}))
+
+;; Note: following will trigger React warning: react-warning-keys. Safe to ignore.
+(defcard edn-card-shared
+  (dc/edn-card app-model))
 
 (defcard om-generic-scrubber
   (dc/om-root-card (partial scrubbable-widget "x-y-view" x-y-viewer) app-model))
 
 (defcard om-sin-view
   (dc/om-root-card (partial scrubbable-widget "sine-view" sin-viewer) app-model))
-
-(defcard edn-card-shared
-  (dc/edn-card app-model))
